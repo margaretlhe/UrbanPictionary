72bc98f505cd2f073496a28a39c072ce3fa4ce72
Jake <39200701+JakeAdler@users.noreply.github.com>
2019-01-04 23:03:08 -0800

    Took out console.logs for socket testing

--- controllers/canvas_manager.js
+++ controllers/canvas_manager.js
@@ -4,18 +4,14 @@ exports.setCanvasManager = function (gamecode) {
     let socket = utils.getSocketConnection(gamecode);
 
     socket.on('connection', function (socket) {
-        console.log("someone has connected!");  
 
         socket.on('drawing', function (dataObj) {
-            console.log('drawing event triggered');
             updateJudgeCanvas(dataObj);
-         //   socket.broadcast.emit('drawingToJudge', dataObj);
         });
 
         socket.on('disconnect', function (socket) {
             console.log('user disconnected');
         });
-        console.log('CANVAS MANAGER');
     });
     
     function updateJudgeCanvas(dataObj){
